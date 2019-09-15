742d7d1a7615594f57f04c9c81171e2c3a693b02
root <shs395@ajou.ac.kr>
2019-09-15 01:28:22 +0000

    develop brnach commit

---
+++ config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  url: 'https://shs395.github.io/',
+  pathPrefix: '/',
+  title: 'IT BLOG',
+  subtitle: 'ss',
+  copyright: '© All rights reserved.',
+  disqusShortname: 'blog-imlodp2whp',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+  ],
+  author: {
+    name: 'Hyunsuk Shin',
+    photo: '/man-user.png',
+    bio: '군인',
+    contacts: {
+      email: 'gdj03094@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'shs395',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: ''
+    }
+  }
+};
---
+++ content/posts/useful-website/index.md
@@ -0,0 +1,8 @@
+---
+title: 유용한 웹사이트
+date: "2019-09-14T20:00:00+09:00"
+description: 알아두면 좋은 사이트 모음
+---
+
+#### website 점수 매겨주는 곳
+https://website.grader.com/
