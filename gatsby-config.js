159120894b70e2f9ca958d405d2e30b380b721c7
shs395 <shs395@ajou.ac.kr>
2021-12-15 11:16:13 +0900

    update google analytics tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,9 +53,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: `UA-62251910-1`,
+        // your google analytics tracking id
+        trackingId: `G-95QK2V5WY2`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
     },
     `gatsby-plugin-feed`,
